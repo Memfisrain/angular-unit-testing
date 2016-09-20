45459b1f6fae1f75d633b2d1638c717f7695d6ef
Memfis <nikitamemfis@gmail.com>
2016-09-20 23:05:57 +0400

    finish testing with Dates

--- spec/movie-app/from-now.filter.spec.js
+++ spec/movie-app/from-now.filter.spec.js
@@ -14,4 +14,11 @@ fdescribe('from-now filter', function () {
 	it('should return same value for invalid date', function() {
 		expect(fromNow('foo')).toBe('foo');
 	});
+
+	it('should return value of years ago fro date Object', function() {
+		var value = new angular.mock.TzDate(0, '2013-07-01T00:00:00.000Z');
+		var baseDate = new angular.mock.TzDate(0, '2015-08-01T20:00:00.000Z');
+
+		expect(fromNow(value, baseDate)).toBe('2 years ago');
+	})
 })
--- src/movie-app/from-now.filter.js
+++ src/movie-app/from-now.filter.js
@@ -1,11 +1,12 @@
 angular.module('movieApp')
 	.filter('fromNow', function() {
-		return function(v) {
+		return function(v, base) {
 			if (v == undefined) {
 				throw new Error('filter-now error');
 			}
 
 			var date = v;
+			var baseDate = base;;
 
 			if (typeof v === 'string') {
 				date = new Date(v);
@@ -13,8 +14,18 @@ angular.module('movieApp')
 
 			if (isNaN(date.getTime())) {
 				return v;
-			} else {
-				return date;
 			}
+
+			var YEAR_IN_MS = 1000 * 60 * 60 * 24 * 365;
+			var now = base || new Date;
+
+			var dateDiff = ((now.getTime() - date.getTime()) / 1000); // in seconds
+			var tzDiff = (now.getTimezoneOffset() - date.getTimezoneOffset()) * 60; // in seconds
+			var diffInS = dateDiff + tzDiff;
+
+			var yearsDiff = Math.floor(diffInS * 1000 / YEAR_IN_MS);
+			console.log(yearsDiff);
+
+			return yearsDiff + ' years ago';
 		}
 	});
